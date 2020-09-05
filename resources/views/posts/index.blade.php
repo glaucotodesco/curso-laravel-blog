5de5543b9fd7cc6044441875ae0559ef6ea0901b
glauco.todesco <glauco.todesco@facens.br>
2020-09-05 18:19:56 -0300

    Aula11-Exercício BootStrap

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 @section('content')
-   <h1>Postagens</h1>    
-    @if(count($posts) > 0)
+    <h1>Postagens</h1>
+    @if (count($posts) > 0)
         @foreach ($posts as $post)
-            <div class="card">
-                <h3>{{$post->title}}</h3>
-                <h5> {{Illuminate\Support\Str::limit($post->body, $limit =5, $end = '...') }}</h5>
-                <small>Data: {{$post->created_at->format('d-m-Y H:i:s')}}</small>
-            </div>    
+            <div class="card mb-3">
+                <div class="card-header">
+                    <a href="/posts/{{$post->id}}">{{ $post->title }}</a>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title"> {{ Illuminate\Support\Str::limit($post->body, $limit = 5, $end = '...') }}</h5>
+                    <small class="card-text">Data: {{ $post->created_at->format('d-m-Y H:i:s') }}</small>
+                </div>
+            </div>
         @endforeach
     @endif
 @endsection
