3fb61afed9d8fcacb2065f2d64b7981a30f6bfc5
glauco.todesco <glauco.todesco@facens.br>
2020-09-05 18:47:11 -0300

    Aula13-Exercicio-OrderBy

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,9 @@ class PostsController extends Controller
      */
     public function index()
     {
-         $posts = Post::all();
+         //$posts = Post::all();
+         //$posts = Post::orderBy('title', 'asc')->get();
+         $posts = Post::orderBy('created_at', 'desc')->get();
          return view('posts.index')->with('posts', $posts);
     }
     
