df5cc4845e54c0e03fca04cebf027883c57f29d5
glauco.todesco <glauco.todesco@facens.br>
2020-09-07 23:31:47 -0300

    Aula23-View User Posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,23 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    @if (count($posts) > 0)
+                    @foreach ($posts as $post)
+                        <div class="card mb-3">
+                            <div class="card-header">
+                                <a href="/posts/{{$post->id}}">{{ $post->title }}</a>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"> {{ Illuminate\Support\Str::limit($post->body, $limit = 5, $end = '...') }}</h5>
+                                <?php //<small class="card-text">Data: {{$post->created_at->format('d-m-Y H:i:s') }}</small> ?>
+                                <small class="card-text">
+                                    Data: {{ date('d-m-Y H:i:s',strtotime(str_replace('-','/', $post->created_at)))}}
+                                </small>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+            
                 </div>
             </div>
         </div>
