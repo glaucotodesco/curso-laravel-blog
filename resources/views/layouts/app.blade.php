7341917eda534b275757c03430931757f02fdc73
glauco.todesco <glauco.todesco@facens.br>
2020-09-07 21:41:33 -0300

    Aula21-Relacionando-Post-Com-IdUser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                       
                             <li class="nav-item">
                                 <a class="nav-link" href="/">Home</a>
                             </li>
@@ -56,9 +56,11 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="/about">Sobre</a>
                             </li>
+                        @guest     
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
+                       
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
